ada6ce961b00062c469ef6a27600e127c59c2d12
Daniloasgit <danilo.a.carvalho6@aluno.senai.br>
2024-07-23 09:55:46 -0300

    instalações npm

---
+++ server.js
@@ -0,0 +1,7 @@
+//importando biblioteca
+
+const express = require('express'); //importa a framework express
+const dotenv = require ('dotenv'); // importa o pacote dotenv para gerenciar variaisveis do ambiente
+const cors = require('cors');//importa o cors para permitir requisiçoes de diferentes origens
+const bodyParser = require('body-parser'); //importa o pacote bodyParser para analisar o corpo das requisições http
+
