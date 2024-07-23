f2f650d66407dab49f78727af5f272dd5c7e8bb0
Daniloasgit <danilo.a.carvalho6@aluno.senai.br>
2024-07-23 10:47:02 -0300

    instalações npm1.1

--- server.js
+++ server.js
@@ -5,3 +5,25 @@ const dotenv = require ('dotenv'); // importa o pacote dotenv para gerenciar var
 const cors = require('cors');//importa o cors para permitir requisiçoes de diferentes origens
 const bodyParser = require('body-parser'); //importa o pacote bodyParser para analisar o corpo das requisições http
 
+dotenv.config(); // carrega as variaveis definidas no arquivo '.env' para process.env(processos)
+
+//iniciando nova aplicação express
+
+const app = express();
+
+// configurar o cors e o bodyParser
+app.use(cors()); // habilita cors para todas as rotas
+app.use(bodyParser.json()); //configura o bodyParser para analisar as requisições json
+
+// Rota inicial para testar o servidor
+
+app.get('/',(require, response)=>{
+    response.send ("servior está ativo") //definir uma rota para testar o servidor
+});
+
+//configurar o servidor para uma porta especifica
+
+ const port = process.env.PORT || 3000  // define a porta a partir da variavel do ambiente ou usa a porta 3000 como padrão
+ app.listen(port,()=>{
+    console.log(`servidor rodando na porta ${port}`) // imprime a mensagem quando o servidor estiver rodando
+ })
